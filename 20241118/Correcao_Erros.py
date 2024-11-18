326da342d7a53b00bfb10d682b3a067c1d7adf72
Sabrina <sabrina.le17@aluno.ifsc.edu.br>
2024-11-18 12:50:53 -0300

    aula_sono_1118

---
+++ 20241118/Correcao_Erros.py
@@ -0,0 +1,13 @@
+#coding utf-8
+
+def Demo_mac():
+    vet = [-4, 12, 0, 3]
+    nome = "xiara"
+    dic = {"Igor": 18,"xiara": 33, "Ana Julia": 16, "Rafael": 21}
+    print(f"O maior valor do vetor é: {max(vet)}")
+    print(f"A letra mais adiantada de {nome}é {max(vet)}")
+    print(f"Metrica no dicionario: {max(dic)}")
+
+if __name__=="__main__":
+Demo_mac()
+
