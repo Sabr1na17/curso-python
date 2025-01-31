dcc231660bd0a01432a5a813ec566f56c9d4d097
sabrina <sabrina.le17@aluno.ifsc.edu.br>
2025-01-31 13:11:27 -0300

    primeira aula de 2025 (21)

---
+++ 20250131/DataHora.py
@@ -0,0 +1,7 @@
+#coding utf-8
+
+import datetime as dt
+def data_hora():
+    momento = str(dt.datetime.now())
+    
+    
---
+++ 20250131/main.py
@@ -0,0 +1,10 @@
+#coding utf-8
+
+import classConta Especial as CE
+import classContaBancaria as CB
+
+ContasBasicas = []
+ContaEspecial = []
+Cb1 = CB.ContasBasica("Ana Julia")
+print("Saldo = 
+Ce1 = CE
