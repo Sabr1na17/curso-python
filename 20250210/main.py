9a03583af21368153893786ed4e3940f4a8579ba
sabrina <sabrina.le17@aluno.ifsc.edu.br>
2025-02-10 13:20:35 -0300

    aula 02 do mes de fevereira

---
+++ 20250210/main.py
@@ -0,0 +1,18 @@
+#coding: utf-8
+'''
+import tkinter as tkinter
+class JanelaPrincipal(tk.Tk):
+    def __init__(self):
+        super.title("Títilo")
+        label = tk.Label(self, bd = 5,
+        text="Texto qualquer",
+        relief = 'groove')
+    label.pack(fill=tk.x,
+        expand=true, padx=10, pady=5)
+
+if __name__ == "__main__":
+    janela = JanelaPrincipal()
+    janela.mainloop()
+    
+ '''
+
