2c2a5806580eb9df0449107f9e91115ca49651c6
sabrina.le17@aluno.ifsc.edu.br <sabrina.le17@aluno.ifsc.edu.br>
2025-02-14 13:06:17 -0300

    aulinha da coluna

--- 20250212/main.py
+++ 20250212/main.py
@@ -1,53 +1,108 @@
 #coding: utf-8
 import tkinter as tk
+import sqlite3 as sq #Importacao do banco de dados. 
 
-class Controlador(tk.Frame):
-    def __init__(self, JanPr):
-        tk.Frame.__init__(self,JanPr)
-        #Chama uma outra classe para construir a pagina sobre a pagina pai.
-        self.PagLogin = PaginaLogin(self)
-        self.PagLogin.grid(row=0, colunm=0)
-
-        #Criacao do menu de tela
-        self.menu = TrocadorTela(self)
-
-
-
-class TrocadorTela(tk.Frame):
-    def __init__(self, JanPr)
-    tk.Frame..__init__(self, JanPr)
-    self.PagLogin = tk.Button(self, text = "login", height = 2, width = 16, command = self.master.PagLogin.tkraise)
-    self.PagLogin.grid(row = 0, column = 0)
-
-class PaginaLogin(tk.Frame):
-    def __init__(self, JanPr):
-        tk.Frame.__init__(self,JanPr)
-        #colocar um label para iniciar a tela de login 
-        self.Label1=tk.Label(self, text = "Tela de Login", height = 5, width = 40, backgroud = "Green")
-          #colocacao de nome de usuario de senha:
-        self.Label1.grid(row = 0, colunm = 0)
-        self.Label_Nome_Usuario = tk.Label(self, text = "Nome do usuario: ", height= 3, width = 15)
-        self.Label_Nome_Usuario.grid(row = 1, column = 0)
-        self.Entry_Nome_Usuario=tk.Entry(self)
-        self.Entry_Nome_Usuario.grid(row=1, column = 1)
-    print(self.Entry_Nome_Usuario)
+def CriacaoBancoDados(): 
+	NomeBD = "Usuario_senha.db"
+	tabelaBD = '''
+		CREATE TABLE IF NOT EXISTS Usuario_senha(
+			nome TEXT NOT NULL,
+			senha TEXT NOT NULL);
+			'''
+	conexao = sq.connect(NomeBD)
+	cursor = conexao.cursor()
+	cursor.execute(tabelaBD)
+	conexao.commit()
+	nome = input("Digite seu nome: ")
+	senha = input("Digite sua senha: ")
+	conexao.execute("INSERT INTO Usuario_senha VALUES (?,?)", (nome, senha))
+	conexao.commit()
+	
+	
+			
 
 
-def main():
-    JanelaPrincipal = tk.Tk()
-    JanelaPrincipal.title("Banco Python")
-    #Criacao de janela com 70% do tamanho da tela.
-    TamHorJanela = int(JanelaPrincipal.winfo_screenwidth()*0.7)
-    TamVerJanela = int(JanelaPrincipal.winfo_screenheigth()*0.7)
-    print(TamHorJanela, TamVerJanela)
-    TamTela = str(TamHorJanela)+"x"+str(TamVerJanela)
-    JanelaPrincipal.geometry(TamTela)
+def __init__(self, JanPr): 
+	class Controlador(tk.Frame):
+		tk.Frame.__init__(self,JanPr)
+		#Chama uma outra classe para construir a pagina sobre a pagina pai.
+		self.PagLogin = PaginaLogin(self)
+		self.PagLogin.grid(row=0, column = 0)
+		
+		#Criacao do menu de tela. 
+		self.menu = TrocadorTela(self)
+		self.menu.grid(row = 1, column = 0)
+		
 
-    Pagina0 = Controlador(JanelaPrincipal)
-    Pagina0.pack(fill=tk.BOTH)
-    JanelaPrincipal.mainloop()
+class TrocadorTela(tk.Frame): 
+	def __init__(self, JanPr): 
+		tk.Frame.__init__(self,JanPr)
+		#Cuidado com o comando
+		self.PagLogin = tk.Button(self, text = "Login", height = 2, width = 16, command = self.master.PagLogin.tkraise)			
+		self.PagLogin.grid(row = 0, column = 0)
+		
+
+class PaginaLogin(tk.Frame): 
+	def __init__(self, JanPr): 
+		tk.Frame.__init__(self,JanPr)
+		#Colocar um label para iniciar a tela de login
+		self.Label1=tk.Label(self, text = "Tela de Login", height = 5, width = 40, 
+			background = "Green")
+		elf.Label1.grid(row = 0, column = 0)
+		#Colocacao de nome dse usuario e senha: 
+		self.Label_Nome_usuario = tk.Label(self, text = "Nome do usuario: ", 
+			height= 3, width = 15)
+		self.Label_Nome_usuario.grid(row=1, column = 0)
+		self.Entry_Nome_usuario=tk.Entry(self)
+		self.Entry_Nome_usuario.grid(row=1, column = 1)
+		self.Label_Senha_usuario = tk.Label(self, text = "Senha do usuario: ", 
+			height= 3, width = 15)
+		self.Label_Senha_usuario.grid(row=2, column = 0)
+		self.Entry_Senha_usuario=tk.Entry(self, show = "*")
+		self.Entry_Senha_usuario.grid(row=2, column = 1)
+		#colocaco do botao
+		self.VerLogin = tk.Button(self, text = "Entrar", bg = "White", height = 2, 
+			width = 15, command = self.VerificaLogin)
+		self.VerLogin.grid(row = 3, column=0)	
+	
+	def VerificaLogin(self): 
+		print("Entrou")		
+		conexao = sq.connect('Usuario_senha.db')
+		cursor=conexao.cursor()
+		Selecao = '''
+			SELECT * FROM Usuario_senha
+			'''
+		cursor.execute(Selecao)	
+		Dados = cursor.fetchall()
+		print(Dados)
+		
+		
+		
 
 
 
-if __name__=="__main__":
-    main()
+			
+
+
+
+
+def main(): 	
+	JanelaPrincipal = tk.Tk()
+	JanelaPrincipal.title("Banco Python")
+	#Criacao de janela com 70% do tamanho da tela. 
+	TamHorJanela = int(JanelaPrincipal.winfo_screenwidth()*0.3)
+	TamVertJanela = int(JanelaPrincipal.winfo_screenheight()*0.6)
+	print(TamHorJanela, TamVertJanela)
+	TamTela = str(TamHorJanela)+"x"+str(TamVertJanela)
+	JanelaPrincipal.geometry(TamTela)
+	Pagina0 = Controlador(JanelaPrincipal)
+	Pagina0.pack(expand = True, fill=tk.BOTH)
+	JanelaPrincipal.mainloop()
+
+if __name__=="__main__": 
+	CriacaoBancoDados()
+	main()
+	
+
+
+
